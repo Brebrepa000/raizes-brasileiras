552d94dd28f1a7a9446c3c3047f7494c25c840df
Brebrepa000 <brenopalomo74@gmail.com>
2025-06-11 19:35:35 -0300

    style: ajustei a cor do botão

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 id='titulo'>
+        Raízes brasileiras
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,5 @@
+export const Card = props => {
+    return (
+        <div className="w-full text-blue-500"><h2>{props.title}</h2></div>
+    )
+}
