eece990ba6b23d2b990d693d536c9d8e9a63e22c
ABOSULTEGEEKGAMER <arnoldperreira2738@gmail.com>
2025-06-13 19:59:42 -0300

    poderia ter me avisado

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import {
   Route,
 } from "react-router-dom";
 import { HomePage } from "./pages/Home"
-<<<<<<< HEAD
 import { SignUpPage } from "./pages/SignUp";
-=======
 import { CreaturesPage } from "./pages/Criaturas";
->>>>>>> 31db25a0f4d80fb069d17162a0afce285ab9a3e9
 
 
 const App = () => {
@@ -16,15 +13,8 @@ const App = () => {
     <BrowserRouter>
       <Routes >
         <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
         <Route path="/cadastro" element={<SignUpPage />} />
-        <Route path="/login" element={<HomePage />} />
-        <Route path="/criaturas" element={<HomePage />} />
-        <Route path="/contos" element={<HomePage />} />
-=======
-        <Route path="/cadastro" element={<HomePage />} />
         <Route path="/criaturas" element={<CreaturesPage />} />
->>>>>>> 31db25a0f4d80fb069d17162a0afce285ab9a3e9
       </Routes>
     </BrowserRouter>
   )
