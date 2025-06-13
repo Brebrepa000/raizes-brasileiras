31db25a0f4d80fb069d17162a0afce285ab9a3e9
Brebrepa000 <brenopalomo74@gmail.com>
2025-06-13 19:54:27 -0300

    Formato de descrição atualizado

--- src/pages/Criaturas/index.jsx
+++ src/pages/Criaturas/index.jsx
@@ -5,10 +5,13 @@ export const  CreaturesPage = () => {
                 <h1 className=" titulo font-serif">Criaturas</h1> 
             </header>
             <main>
-            <p className="importante p1 text-white text-1xl">Nesta área, você vai descobrir um grupo de personagens que habitam as lendas urbanas e o folclore do Brasil. Desde figuras famosas, como a Loira do Banheiro e o Saci-Pererê, até seres menos conhecidos de algumas regiões, cada um deles traz consigo elementos culturais, históricos e simbólicos que revelam receios, crenças e enigmas do nosso dia a dia.
+                <div className=" w-full flex items-center justify-center">
+
+            <p className="importante p1 text-white text-1xl max-w-md text-center bg-transparent">Nesta área, você vai descobrir um grupo de personagens que habitam as lendas urbanas e o folclore do Brasil. Desde figuras famosas, como a Loira do Banheiro e o Saci-Pererê, até seres menos conhecidos de algumas regiões, cada um deles traz consigo elementos culturais, históricos e simbólicos que revelam receios, crenças e enigmas do nosso dia a dia.
 
 Vamos investigar a identidade dessas entidades, a origem de suas narrativas e a maneira como elas evoluíram com o passar dos anos — através de histórias antigas, adaptações atuais e depoimentos individuais.
 </p>
+                </div>
             </main>
         </div>
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,16 +4,20 @@ export const HomePage = () => {
   return (
     <div>
       <header>
-      <h1 className="font-serif titulo">
-        Raízes brasileiras
-      </h1>
+        <h1 className="font-serif titulo">
+          Raízes brasileiras
+        </h1>
       </header>
       <main>
-      <h2 id='sobre' className='text-red-700 flex justify-center text-4xl importante font-serif'>Sobre nós: </h2>
-      <p className='text-white p1 font-serif text-1xl shadow-black'>Aqui você encontra um cantinho especial para as lendas urbanas do Brasil, onde a gente junta e espalha essas histórias. Tem de tudo: casos contados por gente de vários cantos, causos que moram na nossa imaginação. Uns vêm de antigamente, passando de boca em boca, e outros são novinhos em folha. <br />
-A ideia é guardar essas narrativas, entender de onde elas vêm e mostrar o que o povo sente e pensa sobre elas, tanto quem viveu quanto quem só ouviu falar. Não importa se você só gosta de saber, se pesquisa ou se tem algo para contar: sinta-se em casa.
-</p>
-<p>Olá, mundo!</p>
+        <h2 id='sobre' className='text-red-700 flex justify-center text-4xl importante font-serif'>Sobre nós: </h2>
+
+<div className="w-full flex items-center justify-center">
+
+        <p className='text-white p1 font-serif text-1xl shadow-black max-w-md text-center'>Aqui você encontra um cantinho especial para as lendas urbanas do Brasil, onde a gente junta e espalha essas histórias. Tem de tudo: casos contados por gente de vários cantos, causos que moram na nossa imaginação. Uns vêm de antigamente, passando de boca em boca, e outros são novinhos em folha. <br />
+          A ideia é guardar essas narrativas, entender de onde elas vêm e mostrar o que o povo sente e pensa sobre elas, tanto quem viveu quanto quem só ouviu falar. Não importa se você só gosta de saber, se pesquisa ou se tem algo para contar: sinta-se em casa.
+        </p>
+</div>
+        <p>Olá, mundo!</p>
       </main>
       <Footer />
     </div>
